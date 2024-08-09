4959eac81f921a9901245c58f900984d879e2bc4
Prof. - Xadk3! <xadkeg@duck.com>
2024-08-09 23:16:56 +0545

    Fixed zombie QRs

--- qrencode/main.js
+++ qrencode/main.js
@@ -1,10 +1,14 @@
+let QR_GENERATED = 0;
+
 const encode2QR = () => {
-  var qrcode = new QRCode("output", {
+  if (QR_GENERATED) {
+    document.querySelector('img').remove();
+  }; var qrcode = new QRCode("output", {
 	     text: document.querySelector('.qrcontent').value,
 	     width: 128,
 	     height: 128,
 	     colorDark : "#000000",
 	     colorLight : "#ffffff",
 	     correctLevel : QRCode.CorrectLevel.H
-	 }); 
+	 }); QR_GENERATED = 1;
 }
