e802ebaf31624d6d38f3d7f6c463bb1c993930f1
Prof. - Xadk3! <xadkeg@duck.com>
2024-08-10 13:02:13 +0545

    Made qrEncodeR.onKeyPress/Up.

--- qrencode/main.js
+++ qrencode/main.js
@@ -1,14 +1,18 @@
 let QR_GENERATED = 0;
 
+
 const encode2QR = () => {
   if (QR_GENERATED) {
     document.querySelector('img').remove();
-  }; var qrcode = new QRCode("output", {
+  }; let qrcode = new QRCode("output", {
 	     text: document.querySelector('.qrcontent').value,
 	     width: 128,
 	     height: 128,
 	     colorDark : "#000000",
 	     colorLight : "#ffffff",
 	     correctLevel : QRCode.CorrectLevel.H
-	 }); QR_GENERATED = 1;
+	 }); QR_GENERATED = 1; return qrcode
 }
+
+
+document.querySelector('textarea').onkeyup = encode2QR;
